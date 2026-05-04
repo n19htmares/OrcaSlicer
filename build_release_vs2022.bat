ba01ab000cd28cbc110674167346b6a261a74381
n19htmares <smokin.rsx@gmail.com>
2026-05-03 22:24:27 -0700

    Fix build script for Visual Studio 18 2026

--- build_release_vs2022.bat
+++ build_release_vs2022.bat
@@ -1,18 +1,15 @@
 @REM OrcaSlicer build script for Windows
 @echo off
 set WP=%CD%
-
 @REM Pack deps
 if "%1"=="pack" (
     setlocal ENABLEDELAYEDEXPANSION 
     cd %WP%/deps/build
     for /f "tokens=2-4 delims=/ " %%a in ('date /t') do set build_date=%%c%%b%%a
     echo packing deps: OrcaSlicer_dep_win64_!build_date!_vs2022.zip
-
     %WP%/tools/7z.exe a OrcaSlicer_dep_win64_!build_date!_vs2022.zip OrcaSlicer_dep
     exit /b 0
 )
-
 set debug=OFF
 set debuginfo=OFF
 @REM Default target architecture to the host CPU arch; override with x64/arm64 arg.
@@ -41,37 +38,31 @@ if "%debug%"=="ON" (
 )
 if "%arch%"=="ARM64" set build_dir=%build_dir%-arm64
 echo build type set to %build_type%, arch=%arch%
-
 setlocal DISABLEDELAYEDEXPANSION 
 cd deps
 mkdir %build_dir%
 cd %build_dir%
 set "SIG_FLAG="
 if defined ORCA_UPDATER_SIG_KEY set "SIG_FLAG=-DORCA_UPDATER_SIG_KEY=%ORCA_UPDATER_SIG_KEY%"
-
 if "%1"=="slicer" (
     GOTO :slicer
 )
 echo "building deps.."
-
 echo on
 REM Set minimum CMake policy to avoid <3.5 errors
 set CMAKE_POLICY_VERSION_MINIMUM=3.5
-cmake ../ -G "Visual Studio 17 2022" -A %arch% -DCMAKE_BUILD_TYPE=%build_type%
+cmake ../ -G "Visual Studio 18 2026" -A x64 -DCMAKE_BUILD_TYPE=%build_type%
 cmake --build . --config %build_type% --target deps -- -m
 @echo off
-
 if "%1"=="deps" exit /b 0
-
 :slicer
 echo "building Orca Slicer..."
 cd %WP%
 mkdir %build_dir%
 cd %build_dir%
-
 echo on
 set CMAKE_POLICY_VERSION_MINIMUM=3.5
-cmake .. -G "Visual Studio 17 2022" -A %arch% -DORCA_TOOLS=ON %SIG_FLAG% -DCMAKE_BUILD_TYPE=%build_type%
+cmake .. -G "Visual Studio 18 2026" -A x64 -DORCA_TOOLS=ON %SIG_FLAG% -DCMAKE_BUILD_TYPE=%build_type%
 cmake --build . --config %build_type% --target ALL_BUILD -- -m
 @echo off
 cd ..
